be6c97b7824418a219d9534aa28ce474b7bc1194
Chenhua Fan <chenhuafan@Chenhuas-MacBook-Pro.local>
2020-07-13 23:25:01 -0700

    login phase I

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,45 +1,96 @@
 import * as React from 'react';
-import { Typography, Space, Divider, Form, Input, Button, Checkbox, Tabs } from 'antd';
-import { UserOutlined, LockOutlined, GoogleOutlined, PhoneOutlined, NumberOutlined } from '@ant-design/icons';
+import { Typography, Space, Divider, Form, Input, Button, Checkbox, Tabs, Select } from 'antd';
+import { MailOutlined, LockOutlined, GoogleOutlined, PhoneOutlined, NumberOutlined } from '@ant-design/icons';
 import { Store } from 'antd/lib/form/interface';
-
-import styles from '../static/styles/loginLayout.module.css'
 
 const { Title, Text, Link } = Typography;
 const { TabPane } = Tabs;
+const { Option } = Select;
 
+// constant variable
+const
+  PHONE_REGS: any = {
+    "1": /^(1\s?)?(\(\d{3}\)|\d{3})\s?-?\d{3}-?\s?\d{4}$/,
+    "86": /^1[3456789]\d{9}$/
+  },
+  TIME = 120;
+
+// interface
+interface IProps {
+  verifierId: string,
+  onGetCaptcha: (phone: string) => void,
+  onLoginViaEmail: (values: Store) => void,
+  onLoginViaPhone: (values: Store) => void,
+}
 interface IState {
-  login: {
-    type: string
-  }
+  type: string,
+  validPhone: boolean,
+  captchBtnClicked: boolean,
+  area: string,
+  phone: string,
+  counter: number
+}
+interface Login {
+  captchaTimer: any
 }
 
-class Login extends React.Component<{}, IState> {
+class Login extends React.Component<IProps, IState> {
 
-  constructor(props: {}) {
+  public constructor(props: IProps) {
     super(props);
-    this.onFinish.bind(this);
-    this.onTabChange.bind(this);
     this.state = {
-      login: {
-        type: "email"
-      }
+      type: "phone",
+      validPhone: false,
+      captchBtnClicked: false,
+      area: "1",
+      phone: "",
+      counter: TIME
     }
+    this.captchaTimer = null;
   }
 
-  onFinish(values: Store) {
-    console.log('Received values of form: ', values);
+  private onTabChange(key: string): void {
+    this.setState({ "type": key });
   }
 
-  onTabChange(key: string) {
-    this.setState({
-      login: {
-        type: key
+  private onPhoneChange(event: React.ChangeEvent<HTMLInputElement>): void {
+    // update phone;
+    const val = event.target.value;
+    const validPhone = PHONE_REGS[this.state.area.toString()].test(val);
+    this.setState({ "phone": val, "validPhone": validPhone });
+  }
+
+  private onAreaChange(value: string): void {
+    // update area;
+    this.setState({ "area": value });
+  }
+
+  private onClickGetCaptcha(): void {
+    this.setState({ "captchBtnClicked": true });
+    // set timer for TIME s
+    this.captchaTimer = setInterval(() => {
+      if (this.state.counter < 1) {
+        this.setState({ "counter": TIME, "captchBtnClicked": false });
+        clearInterval(this.captchaTimer);
       }
-    })
+      this.setState({ "counter": this.state.counter - 1 });
+      console.log(!(this.state.validPhone && !this.state.captchBtnClicked));
+    }, 1000);
+    this.props.onGetCaptcha(`+${this.state.area}${this.state.phone}`);
   }
 
-  render() {
+  public render() {
+
+    const selectBefore = (
+      <Select
+        defaultValue={this.state.area}
+        className="select-before"
+        onChange={(v) => this.onAreaChange(v)}
+      >
+        <Option value="86">🇨🇳 86</Option>
+        <Option value="1">🇺🇸 1</Option>
+      </Select>
+    );
     const contentList =
     {
       email: (
@@ -47,17 +98,20 @@ class Login extends React.Component<{}, IState> {
           name="normal_login"
           className="login-form"
           initialValues={{ remember: true }}
-          onFinish={this.onFinish}
+          onFinish={this.props.onLoginViaEmail}
         >
           <Form.Item
-            name="username"
-            rules={[{ required: true, message: 'Please input your Username!' }]}
+            name="email"
+            rules={[
+              { required: true, message: '请输入您的邮箱' },
+              {}
+            ]}
           >
-            <Input size="large" prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+            <Input size="large" prefix={<MailOutlined />} type="email" placeholder="邮箱" />
           </Form.Item>
           <Form.Item
             name="password"
-            rules={[{ required: true, message: 'Please input your Password!' }]}
+            rules={[{ required: true, message: '请输入您的密码' }]}
           >
             <Input
               size="large"
@@ -71,87 +125,78 @@ class Login extends React.Component<{}, IState> {
               <Checkbox>保持登录</Checkbox>
             </Form.Item>
           </Form.Item>
-
           <Form.Item>
             <Button size="large" type="primary" htmlType="submit" block>
               登录
             </Button>
           </Form.Item>
-
           <Text>登录遇到了问题？</Text>
           <Link href="https://ant.design" target="_blank">
             找回密码
           </Link>
-
-          <Divider plain>
-            通过第三方平台
-          </Divider>
-
-          <Form.Item>
-            <Button icon={<GoogleOutlined />} size="large" type="primary" htmlType="submit" block>
-              使用 Google 登录
-            </Button>
-          </Form.Item>
         </Form>
       ),
       phone: (
-        <Form
-          name="normal_login"
-          className="login-form"
-          initialValues={{ remember: true }}
-          onFinish={this.onFinish}
-        >
-          <Form.Item
-            name="username"
-            rules={[{ required: true, message: 'Please input your Username!' }]}
+        <div>
+          <Input
+            size="large"
+            prefix={<PhoneOutlined />}
+            addonBefore={selectBefore}
+            value={this.state.phone}
+            onChange={(e) => this.onPhoneChange(e)}
+            placeholder="手机号码"
+            type="number"
+            style={{ "paddingBottom": "24px" }}
+          />
+          <Form
+            name="normal_login"
+            className="login-form"
+            initialValues={{ remember: true }}
+            onFinish={this.props.onLoginViaPhone}
           >
-            <Input size="large" prefix={<PhoneOutlined />} placeholder="手机号码" />
-          </Form.Item>
-          <Form.Item
-            name="password"
-            rules={[{ required: true, message: 'Please input your Password!' }]}
-          >
-            <Input
-              size="large"
-              prefix={<NumberOutlined />}
-              type="text"
-              placeholder="短信验证码"
-              addonAfter={
-                <Button type="text" htmlType="submit">
-                  获取验证码
-                </Button>
-              }
-            />
-          </Form.Item>
-          <Form.Item>
-            <Form.Item name="remember" valuePropName="checked" noStyle>
-              <Checkbox>保持登录</Checkbox>
+            <Form.Item
+              name="captcha"
+              rules={[{ required: true, message: '请输入6位验证码' }]}
+            >
+              <Input
+                size="large"
+                prefix={<NumberOutlined />}
+                maxLength={6}
+                type="tel"
+                placeholder="短信验证码"
+                addonAfter={
+                  <Button type="text"
+                    id={this.props.verifierId}
+                    onClick={() => this.onClickGetCaptcha()}
+                    // TODO: fix bug 在开始计时后 disabled 属性失效。
+                    disabled={!(this.state.validPhone && !this.state.captchBtnClicked)}
+                  >
+                    {
+                      !this.state.captchBtnClicked ? "获取验证码" : `${this.state.counter} 秒`
+                    }
+                  </Button>
+                }
+              />
             </Form.Item>
-          </Form.Item>
-
-          <Form.Item>
-            <Button size="large" type="primary" htmlType="submit" block>
-              登录
-            </Button>
-          </Form.Item>
-
-          <Divider plain>
-            通过第三方平台
-          </Divider>
-
-          <Form.Item>
-            <Button icon={<GoogleOutlined />} size="large" type="primary" htmlType="submit" block>
-              使用 Google 登录
-            </Button>
-          </Form.Item>
-        </Form>
+            <Form.Item>
+              <Form.Item name="remember" valuePropName="checked" noStyle>
+                <Checkbox>保持登录</Checkbox>
+              </Form.Item>
+            </Form.Item>
+            <Form.Item>
+              <Button size="large" type="primary" htmlType="submit" disabled={!this.state.captchBtnClicked} block>
+                登录
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
       )
     }
 
     return (
       <Space direction="vertical" style={{ width: "100%" }}>
 
-        <div className={styles.center} style={{ "paddingBottom": "48px" }}>
+        <div style={{ "paddingBottom": "48px" }}>
           <Title>登录</Title>
           <Text>没有账户？ 马上</Text>
           <Link href="https://ant.design" target="_blank">
@@ -167,7 +212,12 @@ class Login extends React.Component<{}, IState> {
             {contentList.phone}
           </TabPane>
         </Tabs>
-
+        <Divider plain>
+          通过第三方平台
+        </Divider>
+        <Button icon={<GoogleOutlined />} size="large" type="primary" block>
+          使用 Google 登录
+        </Button>
       </Space>
     );
   }
--- src/containers/LoginLayout.tsx
+++ src/containers/LoginLayout.tsx
@@ -1,21 +1,83 @@
 import * as React from 'react';
 import Login from '../components/Login';
 import { Row, Col } from 'antd';
-
 import styles from '../static/styles/loginLayout.module.css'
-console.log(styles);
+import firebase from '../config/firebase'
+import { Store } from 'antd/lib/form/interface';
 
-interface IProps {
+// constant variable
+const VERIFIED_ID = 'get-captcha-button';
 
+// interface
+declare global {
+  interface Window {
+    confirmationResult: any;
+  }
+}
+interface LoginContainer {
+  confirmationResult: any;
 }
 
-class LoginContainer extends React.Component<IProps, {}> {
-//
+// set firebase user watcher
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    console.log(user.uid);
+    // TODO: login phase II.
+  } else {
+    // User is signed out.
+    // TODO: logout phase II.
+  }
+});
+
+class LoginContainer extends React.Component<{}, {}> {
+
+  public constructor(props: {}) {
+    super(props);
+    this.confirmationResult = null;
+  }
+
+  private onLoginViaEmail(values: Store) {
+    firebase.auth().signInWithEmailAndPassword(values.email, values.password).catch(error => {
+      // TODO: Handle Errors here.
+      console.error(error.code, error.message)
+    });
+  }
+
+  private onGetCaptcha(phone: string) {
+    // init phone verifier
+    const recaptchaVerifier = new firebase.auth.RecaptchaVerifier(VERIFIED_ID, {
+      'size': 'invisible',
+      'callback': function (response: any) {
+        console.log("已验证");
+      }
+    });
+    firebase.auth().signInWithPhoneNumber(phone, recaptchaVerifier)
+      .then((confirmationResult: firebase.auth.ConfirmationResult) => {
+        this.confirmationResult = confirmationResult;
+      }).catch(error => {
+        // TODO: Handle Error; SMS not sent
+        console.error(error.code, error.message)
+      });
+  }
+
+  private onLoginViaPhone(values: Store) {
+    this.confirmationResult.confirm(values.captcha).catch((error: any) => {
+      // TODO: User couldn't sign in (bad verification code?)
+      console.error(error.code, error.message)
+    });
+  }
+
   public render() {
     return (
       <Row justify="center">
-        <Col xs={24} sm={24} md={8} lg={6} className={styles.wrapper}>
-          <Login></Login>
+        <Col xs={24} sm={24} md={12} lg={8} className={styles.wrapper}>
+          <Login
+            verifierId={VERIFIED_ID}
+            onGetCaptcha={(phone) => this.onGetCaptcha(phone)}
+            onLoginViaEmail={(values: Store) => this.onLoginViaEmail(values)}
+            onLoginViaPhone={(values: Store) => this.onLoginViaPhone(values)}
+          ></Login>
         </Col>
       </Row>
     );
